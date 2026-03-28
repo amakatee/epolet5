b73d330c11044b5ae4f3a43d960d055e511b8952
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2026-03-28 23:08:06 +0300

    fix sanity

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const nextConfig = {
         },
       ],
     },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\/sanity\//,
+        loader: 'ignore-loader',
+      });
+      return config;
+    },
   }
   
   module.exports = nextConfig
